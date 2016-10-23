1f9a95667544af6d96f60c974c4b0e0c2a8e9aba
dtstuff9 <dtstuff9@hotmail.com>
2016-10-23 18:36:08 -0400

    web

---
+++ PhotoMosaic_files/photoMosaic.js
@@ -0,0 +1,165 @@
+var PMalbum = [
+    {
+        "src" : "https://c2.staticflickr.com/2/1484/24398335604_d1f35177ed_h.jpg",
+        "caption" : "Leslieville",
+        "width" : 1600,
+        "height" : 973
+    },
+    {
+        "src" : "https://c2.staticflickr.com/2/1524/24586028131_564897616b_b.jpg",
+        "caption" : "Glass Tower",
+        "width" : 1600,
+        "height" : 1076
+    },
+    {
+        "src" : "https://c2.staticflickr.com/2/1688/24658815305_736bedb559_h.jpg",
+        "caption" : "To the West",
+        "width" : 1600,
+        "height" : 777
+    },
+    {
+        "src" : "https://c2.staticflickr.com/6/5699/23594846105_7846c0ca82_h.jpg",
+        "caption" : "The Wing Back",
+        "width" : 1600,
+        "height" : 917
+    },
+    {
+        "src" : "https://c2.staticflickr.com/2/1679/23821184623_ba89a15bd1_b.jpg",
+        "caption" : "Rosedale Station",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7365/16505929126_9658cab40e_b.jpg",
+        "caption" : "Global Rainbow",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8604/16368033232_38e1f694aa_h.jpg",
+        "caption" : "Made in China",
+        "width" : 1149,
+        "height" : 1600
+    },
+    {
+        "src" : "https://c2.staticflickr.com/4/3887/15335581802_6ab7e96c2e_h.jpg",
+        "caption" : "Gazebo on the Lake",
+        "width" : 1600,
+        "height" : 945
+    },
+    {
+        "src" : "https://c1.staticflickr.com/3/2918/14137658740_ac412bdcbb_h.jpg",
+        "caption" : "Peter Gilgan Centre for Research and Learning",
+        "width" : 1067,
+        "height" : 1600
+    },
+    {
+        "src" : "https://c2.staticflickr.com/6/5157/14283197961_77b7f425b0_b.jpg",
+        "caption" : "Commerce Court North",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7309/13934229721_a393f69ab9_h.jpg",
+        "caption" : "Four Generations of TTC Streetcars",
+        "width" : 1600,
+        "height" : 859
+    },
+    {
+        "src" : "https://c2.staticflickr.com/4/3790/11373201385_9fe9d896e8_h.jpg",
+        "caption" : "Snow Angel",
+        "width" : 1323,
+        "height" : 1600
+    },
+    {
+        "src" : "https://c2.staticflickr.com/6/5489/9154563591_ac61793b25_h.jpg",
+        "caption" : "Lamborghini Gallardo LP560-4 Spyder",
+        "width" : 1600,
+        "height" : 965
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8113/8964121851_d7851b83ce_h.jpg",
+        "caption" : "R. C. Harris Water Treatment Plant",
+        "width" : 1045,
+        "height" : 1600
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8371/8556546766_8d3acc6f66_h.jpg",
+        "caption" : "Tobogganing down Riverdale",
+        "width" : 1600,
+        "height" : 953
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8079/8258088129_0f19735cbb_h.jpg",
+        "caption" : "round round get around",
+        "width" : 1600,
+        "height" : 909
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8485/8222107835_5c4c095148_b.jpg",
+        "caption" : "Canoes, Kayaks, and a Tall Ship",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8458/8063909331_d357313f80_b.jpg",
+        "caption" : "Cherry Street Strauss Trunnion Bascule Bridge",
+        "width" : 1024,
+        "height" : 677
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8309/8039317759_55d6d10145_b.jpg",
+        "caption" : "Flat Space",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7270/7688760316_afd13cf1bd_b.jpg",
+        "caption" : "Cinesphere",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8427/7587504324_42889e37c0_b.jpg",
+        "caption" : "Downtown Toronto",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8010/7498337254_599ab33056_b.jpg",
+        "caption" : "Sherbourne Common",
+        "width" : 1024,
+        "height" : 654
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7090/7356121574_62661da13c_b.jpg",
+        "caption" : "Pedestrian Bridge",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7236/7377180248_e350b26790_b.jpg",
+        "caption" : "Irish Pubs",
+        "width" : 1024,
+        "height" : 623
+    },
+    {
+        "src" : "https://c1.staticflickr.com/9/8019/7284071594_f6d0015482_b.jpg",
+        "caption" : "Winter Garden Theatre",
+        "width" : 1024,
+        "height" : 683
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7216/7282060856_aa96623654_b.jpg",
+        "caption" : "Longhouse",
+        "width" : 714,
+        "height" : 1024
+    },
+    {
+        "src" : "https://c2.staticflickr.com/8/7224/7277196704_85b1024b5b_b.jpg",
+        "caption" : "Thomas Fisher Rare Book Library",
+        "width" : 748,
+        "height" : 1024
+    }
+
+];
